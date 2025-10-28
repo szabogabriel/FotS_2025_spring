dd00ae890aae59efbd05120a14de88e160a59c6a
Viktor Jandak <viktor.jandak@sk.ibm.com>
2025-10-28 13:17:33 +0100

    @EnableTransactionManagement @EnableJpaRepositories

--- src/main/java/com/ibm/sk/fots/spring/TaskToolApplication.java
+++ src/main/java/com/ibm/sk/fots/spring/TaskToolApplication.java
@@ -2,9 +2,11 @@ package com.ibm.sk.fots.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@EnableJpaRepositories
 @SpringBootApplication
 public class TaskToolApplication {
 
