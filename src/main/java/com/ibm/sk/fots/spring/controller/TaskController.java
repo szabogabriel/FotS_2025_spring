b039ddadd6b43f79f0176fc28d7cc7ce8261204e
Mykhailo Pavlov <amishapavloffl@gmail.com>
2025-11-02 10:58:23 +0000

    feat(task): add endpoint to list all tasks

--- src/main/java/com/ibm/sk/fots/spring/controller/TaskController.java
+++ src/main/java/com/ibm/sk/fots/spring/controller/TaskController.java
@@ -1,14 +1,25 @@
 package com.ibm.sk.fots.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.ibm.sk.fots.spring.dto.Task;
 import com.ibm.sk.fots.spring.dto.TaskCreate;
 import com.ibm.sk.fots.spring.dto.TaskUpdate;
 import com.ibm.sk.fots.spring.service.ControllerService;
+
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/api/v1/tasks", produces = "application/json")
@@ -53,4 +64,8 @@ public class TaskController {
     return ret;
   }
 
+  @GetMapping
+  public List<Task> findAll() {
+    return service.findAllTasks();
+  }
 }
--- src/main/java/com/ibm/sk/fots/spring/service/ControllerService.java
+++ src/main/java/com/ibm/sk/fots/spring/service/ControllerService.java
@@ -13,6 +13,7 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -154,5 +155,10 @@ public class ControllerService {
       throw new IllegalArgumentException();
     }
     return ret;
+  }
+
+  public List<Task> findAllTasks() {
+    List<TaskEntity> tasks = taskRepo.findAll();
+    return TaskTagMapper.INSTANCE.toDtoListTask(tasks);
   }
 }
