9137d8e4bb2e6c306df95defaea82c37969b5d71
Viktor Barczi <Viktor.Barczi1@ibm.com>
2025-11-04 10:19:08 +0100

    populate other fields of taskEntity

--- src/test/java/com/ibm/sk/fots/spring/config/TestRepositoryConfig.java
+++ src/test/java/com/ibm/sk/fots/spring/config/TestRepositoryConfig.java
@@ -1,5 +1,9 @@
 package com.ibm.sk.fots.spring.config;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import com.ibm.sk.fots.spring.dto.PriorityEnum;
 import com.ibm.sk.fots.spring.entity.TaskEntity;
 import com.ibm.sk.fots.spring.repository.TagRepository;
 import com.ibm.sk.fots.spring.repository.TaskRepository;
@@ -7,33 +11,51 @@ import com.ibm.sk.fots.spring.service.ControllerService;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import java.util.Optional;
+// String testTitle = "Test Title";
+// String testDescription = "Test Description";
+// String testDueDate = "2024-12-31T23:59:59";
+// boolean testCompleted = false;
+// PriorityEnum testPriority = PriorityEnum.MEDIUM;
 
 @Configuration
 public class TestRepositoryConfig {
 
-    @Bean
-    public TaskRepository getTaskRepository() {
-        TaskEntity taskEntity = new TaskEntity();
-        taskEntity.setTaskId(1234L);
-        // TODO populate other fields of taskEntity so that mapper doesn't throw NPE
+  @Bean
+  public TaskRepository getTaskRepository() {
+    //  Setting up mock data
+    Long testId = 1234L;
+    String testTitle = "Test Title";
+    String testDescription = "Test Description";
+    String testDueDate = "2024-12-31T23:59:59";
+    boolean testCompleted = false;
+    PriorityEnum testPriority = PriorityEnum.MEDIUM;
 
-        Optional<TaskEntity> optionalTaskEntity = Optional.of(taskEntity);
-        TaskRepository mockedRepository = Mockito.mock(TaskRepository.class);
-        Mockito.when(mockedRepository.findByTaskId(1234L)).thenReturn(optionalTaskEntity);
+    //  Mocking the repository response, since the TaskEntity does not have a AllArgsConstructor, we set the fields manually
+    TaskEntity taskEntity = new TaskEntity();
+    taskEntity.setTaskId(testId);
+    taskEntity.setTitle(testTitle);
+    taskEntity.setDescription(testDescription);
+    taskEntity.setDueDate(LocalDateTime.parse(testDueDate));
+    taskEntity.setCompleted(testCompleted);
+    taskEntity.setPriority(testPriority.toInteger());
+    taskEntity.setTags(List.of());
+    taskEntity.setOwner(1L);
 
-        return mockedRepository;
-    }
+    Optional<TaskEntity> optionalTaskEntity = Optional.of(taskEntity);
+    TaskRepository mockedRepository = Mockito.mock(TaskRepository.class);
+    Mockito.when(mockedRepository.findByTaskId(1234L)).thenReturn(optionalTaskEntity);
 
-    @Bean
-    public TagRepository tagRepository() {
-        return Mockito.mock(TagRepository.class);
-    }
+    return mockedRepository;
+  }
 
-    @Bean
-    public ControllerService controllerService() {
-        return new ControllerService();
-    }
+  @Bean
+  public TagRepository tagRepository() {
+    return Mockito.mock(TagRepository.class);
+  }
+
+  @Bean
+  public ControllerService controllerService() {
+    return new ControllerService();
+  }
 }
--- src/test/java/com/ibm/sk/fots/spring/service/ControllerServiceContextConfigTest.java
+++ src/test/java/com/ibm/sk/fots/spring/service/ControllerServiceContextConfigTest.java
@@ -1,25 +1,12 @@
 package com.ibm.sk.fots.spring.service;
 
 
+import static org.junit.jupiter.api.Assertions.*;
 import com.ibm.sk.fots.spring.config.TestRepositoryConfig;
-import com.ibm.sk.fots.spring.dto.PriorityEnum;
-import com.ibm.sk.fots.spring.entity.TaskEntity;
-import com.ibm.sk.fots.spring.repository.TaskRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
